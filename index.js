911d2b96e1a4a16e1b25c03dc2a6733d1390ab34
Adam <adam@mac.attlocal.net>
2019-08-04 21:03:42 -0500

    First Commit

---
+++ index.js
@@ -0,0 +1,11 @@
+/*global window*/
+/*global document*/
+/*global Modernizr*/
+/*global SmoothScroll*/
+/*global console*/
+/*global $*/
+/*jshint esversion:6*/
+
+window.onload = () => {
+	console.log("hello world");
+};
