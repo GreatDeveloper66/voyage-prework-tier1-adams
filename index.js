85711795415c41b3831ede2b09365e05bd051a8f
Adam <adam@mac.attlocal.net>
2019-08-18 18:37:52 -0500

    Added and included scroll.js file

--- index.js
+++ index.js
@@ -5,21 +5,24 @@
 /*global console*/
 /*global $*/
 /*jshint esversion:6*/
+/*global event*/
+/*import scrollIt from "scroll.js";*/
 
-
-
-window.onload = () => {s
+window.onload = () => {
 	const toggleDropDown = () => {
-		
 		let navmenu = document.querySelector("menu.navmenu");
 		navmenu.classList.toggle("slidedown");
 		navmenu.classList.toggle("slideup");
 	};
+	
 
 	document.getElementById("accordian").addEventListener("click", toggleDropDown);
 
-	Array.from(document.querySelectorAll("menu.navmenu a button")).forEach(button => {
-		button.addEventListener("click", toggleDropDown);
+	Array.from(document.querySelectorAll("menu.navmenu a")).forEach(a => {
+		a.addEventListener("click", function() {
+			toggleDropDown();
+		});
 	});
 	
+
 };
