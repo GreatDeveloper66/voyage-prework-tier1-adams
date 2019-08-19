c53a3a69b64ebc093c41d21ca2b8e4a8a50dc3b5
Adam <adam@mac.attlocal.net>
2019-08-19 12:54:02 -0500

    Added crude scroll function

--- index.js
+++ index.js
@@ -6,6 +6,7 @@
 /*global $*/
 /*jshint esversion:6*/
 /*global event*/
+/*global requestAnimationFrame*/
 /*import scrollIt from "scroll.js";*/
 
 window.onload = () => {
@@ -14,15 +15,44 @@ window.onload = () => {
 		navmenu.classList.toggle("slidedown");
 		navmenu.classList.toggle("slideup");
 	};
-	
 
 	document.getElementById("accordian").addEventListener("click", toggleDropDown);
 
 	Array.from(document.querySelectorAll("menu.navmenu a")).forEach(a => {
-		a.addEventListener("click", function() {
+		a.addEventListener("click", function () {
 			toggleDropDown();
+			/*event.preventDefault();
+			window.alert(this.getAttribute("href"));*/
+			smoothScroll(this.getAttribute("href"));
 		});
 	});
-	
+
+	function smoothScroll(target = "#homepage", duration = 1000) {
+		target = document.querySelector(target);
+		var targetPosition = target.getBoundingClientRect().top - 75;
+		var startPosition = window.pageYOffset;
+		var distance = targetPosition - startPosition;
+		var startTime = null;
+
+		function animation(currentTime) {
+			if (startTime === null) startTime = currentTime;
+			var timeElapsed = currentTime - startTime;
+			var run = ease(timeElapsed, startPosition, targetPosition, duration);
+			window.scrollTo(0, run);
+			if (timeElapsed < duration) requestAnimationFrame(animation);
+		}
+
+		function ease(t, b, c, d) {
+			t /= d / 2;
+			if (t < 1) return c / 2 * t * t + b;
+			t--;
+			return -c / 2 * (t * (t - 2) - 1) + b;
+		}
+		requestAnimationFrame(animation);
+	}
+
+
+
+
 
 };
--- scroll.js
+++ scroll.js
@@ -8,6 +8,29 @@
 /*global $*/
 /*jshint esversion:6*/
 
+function smoothScroll(target = "#homepage",duration=1000){
+	target = document.querySelector(target);
+	var targetPosition = target.getBoundingClientRect().top;
+	var startPosition = window.pageYOffset;
+	var distance = targetPosition - startPosition;
+	var startTime = null;
+	function animation(currentTime){
+		if(startTime === null) startTime = currentTime;
+			var timeElapsed = currentTime - startTime;
+			var run = ease(timeElapsed, startPosition,distance,duration);
+			window.scrollTo(0,run);
+			if(timeElapsed < duration) requestAnimationFrame(animation);
+	}
+		
+		function ease(t, b, c, d){
+			t /= d/2;
+			if(t < 1) return c / 2 * t * t + b;
+			t--;
+			return -c / 2 * (t * (t - 2) - 1) + b;
+		}
+	requestAnimationFrame(animation);
+}
+/*
 function scrollIt(destination, callback, duration = 200, easing = 'linear') {
 
   const easings = {
@@ -86,3 +109,4 @@ function scrollIt(destination, callback, duration = 200, easing = 'linear') {
 
   scroll();
 }
+*/
