59df78de69e8a9e6fd60417410f8aba4d21d1950
Adam <adam@mac.attlocal.net>
2019-08-20 10:16:16 -0500

    moved smoothScroll to scroll.js to import from seperate file

--- index.js
+++ index.js
@@ -7,7 +7,7 @@
 /*jshint esversion:6*/
 /*global event*/
 /*global requestAnimationFrame*/
-/*import scrollIt from "scroll.js";*/
+import smoothScroll from '/scroll.js';
 
 window.onload = () => {
 	const toggleDropDown = () => {
@@ -16,32 +16,6 @@ window.onload = () => {
 		navmenu.classList.toggle("slideup");
 	};
 	
-	function smoothScroll(target = "#homepage", duration = 1000) {
-		target = document.querySelector(target);
-		var targetPosition = target.getBoundingClientRect().top - 75;
-		var startPosition = window.pageYOffset;
-		var distance = targetPosition - startPosition;
-		var startTime = null;
-
-		function animation(currentTime) {
-			if (startTime === null) startTime = currentTime;
-			var timeElapsed = currentTime - startTime;
-			var run = ease(timeElapsed, startPosition, targetPosition, duration);
-			window.scrollTo(0, run);
-			if (timeElapsed < duration) requestAnimationFrame(animation);
-		}
-
-		function ease(t, b, c, d) {
-			t /= d / 2;
-			if (t < 1) return c / 2 * t * t + b;
-			t--;
-			return -c / 2 * (t * (t - 2) - 1) + b;
-		}
-		requestAnimationFrame(animation);
-	}
-
-	
-
 	document.getElementById("accordian").addEventListener("click", toggleDropDown);
 
 	Array.from(document.querySelectorAll("menu.navmenu a")).forEach(a => {
