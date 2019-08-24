7e282848c2a7cf88452074fcad9c2101145d7485
Adam <adam@mac.attlocal.net>
2019-08-24 10:40:44 -0500

    Updates index.css, index.html, index.js and scroll.js to match reorg branch

--- js/index.js
+++ js/index.js
@@ -7,9 +7,29 @@
 /*jshint esversion:6*/
 /*global event*/
 /*global requestAnimationFrame*/
-import smoothScroll from '/scroll.js';
+import smoothScroll from '/js/scroll.js';
 
 window.onload = () => {
+function setTranslate(xPos, yPos, el) {
+		el.style.transform = "translate3d(" + xPos + ", " + yPos + "px, 0)";
+	}
+	
+	var xScrollPosition;
+	var yScrollPosition;
+
+	function scrollLoop() {
+		xScrollPosition = window.scrollX;
+		yScrollPosition = window.scrollY;
+		setTranslate(0, yScrollPosition * -0.1, document.querySelector(".headerbackground"));
+		setTranslate(0, yScrollPosition * -0.1, document.querySelector(".contactbackground"));
+		setTranslate(0, yScrollPosition * -0.1, document.querySelector(".servicesbackground"));
+		/*
+		setTranslate(0, yScrollPosition * -1.5, blueSquare);
+		setTranslate(0, yScrollPosition * .5, greenPentagon);
+		*/
+
+		requestAnimationFrame(scrollLoop);
+	}
 	const toggleDropDown = () => {
 		let navmenu = document.querySelector("menu.navmenu");
 		navmenu.classList.toggle("slidedown");
@@ -33,5 +53,7 @@ window.onload = () => {
 		const upArrow = document.querySelector("button#slider");
 		upArrow.style.display = window.scrollY < 450 ? "none" : "block";
 		upArrow.style.bottom = window.scrollY > 3700 ? "80px" : "30px";
+		scrollLoop();
+		
 	});
 };
